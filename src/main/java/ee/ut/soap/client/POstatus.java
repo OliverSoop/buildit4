01453bbf5d11e51939678880a519a0729aa04236
Risto <ristos@ut.ee>
2013-12-03 18:19:47 +0200

    PO statuses

--- src/main/java/ee/ut/soap/client/POstatus.java
+++ src/main/java/ee/ut/soap/client/POstatus.java
@@ -1,15 +1,17 @@
-
 package ee.ut.soap.client;
 
 import javax.xml.bind.annotation.XmlEnum;
 import javax.xml.bind.annotation.XmlType;
 
-
 /**
- * <p>Java class for pOstatus.
- * 
- * <p>The following schema fragment specifies the expected content contained within this class.
  * <p>
+ * Java class for pOstatus.
+ * 
+ * <p>
+ * The following schema fragment specifies the expected content contained within
+ * this class.
+ * <p>
+ * 
  * <pre>
  * &lt;simpleType name="pOstatus">
  *   &lt;restriction base="{http://www.w3.org/2001/XMLSchema}string">
@@ -21,6 +23,11 @@ import javax.xml.bind.annotation.XmlType;
  *     &lt;enumeration value="REJECTED"/>
  *     &lt;enumeration value="CLOSED"/>
  *     &lt;enumeration value="CANCELLATION_REQUESTED"/>
+ *     &lt;enumeration value="DISPATCHED"/>
+ *     &lt;enumeration value="DELIVERED"/>
+ *     &lt;enumeration value="REJECTED_BY_CUSTOMER"/>
+ *     &lt;enumeration value="RETURNED"/>
+ *     &lt;enumeration value="INVOICED"/>
  *   &lt;/restriction>
  * &lt;/simpleType>
  * </pre>
@@ -30,21 +37,14 @@ import javax.xml.bind.annotation.XmlType;
 @XmlEnum
 public enum POstatus {
 
-    PENDING_CONFIRMATION,
-    PENDING_UPDATE,
-    UPDATE_REJECTED,
-    OPEN,
-    RECIEVED,
-    REJECTED,
-    CLOSED,
-    CANCELLATION_REQUESTED;
+	PENDING_CONFIRMATION, PENDING_UPDATE, UPDATE_REJECTED, OPEN, RECIEVED, REJECTED, CLOSED, CANCELLATION_REQUESTED, DISPATCHED, DELIVERED, REJECTED_BY_CUSTOMER, RETURNED, INVOICED;
 
-    public String value() {
-        return name();
-    }
+	public String value() {
+		return name();
+	}
 
-    public static POstatus fromValue(String v) {
-        return valueOf(v);
-    }
+	public static POstatus fromValue(String v) {
+		return valueOf(v);
+	}
 
 }
